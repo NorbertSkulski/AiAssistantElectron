f379766990a6a2ee4bf5324025ce808bc773e37d
Norbert <norbert.skulski@biuromax-balcer.pl>
2026-04-18 21:18:57 +0200

    feat: initial commit

--- src/react/App.jsx
+++ src/react/App.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.scss';
 import settingsDb from './apis/indexedDb/settingsDb';
+import Menu from './components/menu/Menu';
 
 const App = () => {
 
@@ -8,9 +9,9 @@ const App = () => {
 
     useEffect(() => {
         const loadSettings = async () => {
-            const settingMenu = await settingsDb.settings.get({ key: 'menuOpen' });
+            const settingMenu = await settingsDb.settings.get({ key: 'menuClose' });
             if (settingMenu && Boolean(settingMenu?.value)) {
-                ref?.current?.classList?.add('OpenMenu');
+                ref?.current?.classList?.add('CloseMenu');
                 return;
             }
         }
@@ -18,18 +19,28 @@ const App = () => {
     }, [])
 
     const toggleMenu = async () => {
-        const settingMenu = await settingsDb.settings.get({ key: 'menuOpen' });
+        const settingMenu = await settingsDb.settings.get({ key: 'menuClose' });
         if (!settingMenu) {
-            await settingsDb.settings.add({ key: 'menuOpen', value: true });
+            await settingsDb.settings.add({ key: 'menuClose', value: true });
         } else {
             await settingsDb.settings.update(settingMenu.id, { value: !settingMenu.value });
         }
-        ref.current.classList.toggle('OpenMenu');
+        ref.current.classList.toggle('CloseMenu');
     }
 
+    const handleAction = (formData,e) => {
+        const query = formData.get("query");
+        console.log(`You searched for '${query}'`);
+        console.log('e',formData)
+    }
+
+    const [query, setQuery] = useState("");
+
+    
     return (
         <div className='MainWindow' ref={ref}>
-            <div className='MenuArea'>menu
+            <div className='MenuArea'>
+                <Menu/>
                 <button onClick={toggleMenu}>click</button>
 
             </div>
@@ -39,6 +50,11 @@ const App = () => {
                 <p>Witaj w aplikacji AI Assistant!</p>
                 <p>Node: {window?.api?.node()}</p>
                 <p>test: {env.TEST}</p>
+
+                <form action={handleAction}>
+                    <input name="query" value={query} onChange={(e) => setQuery(e.target.value)}/>
+                    <button type="submit">Search</button>
+                </form>
 
             </div>
         </div>
---
+++ src/react/components/menu/Menu.jsx
@@ -0,0 +1,9 @@
+
+
+const Menu = () => {
+    return (
+        <div className="bg-red-500 flex items-center justify-center">test menu</div>
+    )
+}
+
+export default Menu;
